1522579cec5a7fe5233c2491fd8a9fecabec516d
wsxingjun <1185889308@qq.com>
2017-10-23 14:12:57 +0800

    v1:增加了子线程来处理网络请求内容，但是仍然在报异常android.os.NetworkOnMainThreadException

--- app/src/main/java/layoutdemo/com/example/a1netsourcecodeinsight/MainActivity.java
+++ app/src/main/java/layoutdemo/com/example/a1netsourcecodeinsight/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -32,34 +33,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv_performCode = (TextView) findViewById(R.id.tv_performCode);
         //2.设置点击事件
         bt_searchURL.setOnClickListener(this);
+        System.out.println("onCreate:"+Thread.currentThread().toString());
 
     }
 
     @Override
     public void onClick(View view) {
         //3.获取输入的地址
-        String urlString = et_url.getText().toString().trim();
+        final String urlString = et_url.getText().toString().trim();
+        System.out.println("onClick:"+Thread.currentThread().toString());
         //4.查询获取的地址的内容
         /*
         * 需要使用到UrlConnetion类；
         * */
         try {
-            //4.1创建一个url对象
-            URL url = new URL(urlString);
-            //4.2获取一个urlConnection对象
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            //4.3 为HttpURLConnection对象设置一些参数，请求方式，连接的超时时间
-            connection.setRequestMethod("GET");
-            System.out.println("11111111");
-            connection.setReadTimeout(1000);
-            //4.4 获取响应码
-            int code = connection.getResponseCode();
-            if (code == 200){
-                //5.获取有效的数据，并将数据解析成为String
-                InputStream stream = connection.getInputStream();
-                String result = StreamUtil.streamToString(stream);
-                tv_performCode.setText(result);
-            }
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    System.out.println("onClick:run()"+Thread.currentThread().toString());
+                    try {
+                        //4.1创建一个url对象
+                        URL url = new URL(urlString);
+                        //4.2获取一个urlConnection对象
+                        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                        //4.3 为HttpURLConnection对象设置一些参数，请求方式，连接的超时时间
+                        connection.setRequestMethod("GET");
+                        System.out.println("11111111");
+                        connection.setReadTimeout(1000);
+                        //4.4 获取响应码
+                        int code = connection.getResponseCode();
+                        if (code == 200){
+                            //5.获取有效的数据，并将数据解析成为String
+                            InputStream stream = connection.getInputStream();
+                            String result = StreamUtil.streamToString(stream);
+                            tv_performCode.setText(result);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).start();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
